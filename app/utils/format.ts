8a9f381bdc77f131536212b919d3166eb710a8ef
pengbo <1223577600@qq.com>
2023-05-24 12:07:50 +0800

    修改代码的错误提示信息

--- app/utils/format.ts
+++ app/utils/format.ts
@@ -1,14 +1,15 @@
 export function prettyObject(msg: any) {
+  console.log("【这是返回的错误信息】", msg);
   if (typeof msg !== "string") {
-    msg = JSON.stringify(msg, null, "  ");
-  } else if (msg.includes("account/api-keys")) {
-    msg = JSON.stringify(
-      "填写的KEY有误，请检查key前后是否有空格或者输入错误！",
-      null,
-      "  ",
-    );
+    if (msg.includes("account/api-keys")) {
+      msg = JSON.stringify(
+        "填写的KEY有误，请检查key前后是否有空格或者输入错误！",
+      );
+    } else {
+      msg = JSON.stringify(msg, null, "  ");
+    }
   }
-  console.log(msg);
+
   const prettyMsg = ["```json", msg, "```"].join("\n");
   return prettyMsg;
 }
